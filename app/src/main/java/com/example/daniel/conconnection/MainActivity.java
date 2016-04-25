926570c4da791dab1237d42ef8687145d1035579
ianberger <spartan1179893@gmail.com>
2016-04-25 02:02:16 -0700

    Changed MainActivity so that yes and no buttons are on the right and left side of the screen. Added listeners to buttons to make them functional.

--- app/src/main/java/com/example/daniel/conconnection/MainActivity.java
+++ app/src/main/java/com/example/daniel/conconnection/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.daniel.conconnection;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button yesButton = (Button) findViewById(R.id.yesButton);
+        Button noButton = (Button) findViewById(R.id.noButton);
+        final ImageView potentialMatchPicture = (ImageView) findViewById(R.id.potentialMatchPicture);
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO make as a match in database, get next potential match picture and info
+                potentialMatchPicture.setImageResource(R.mipmap.ic_launcher);
+            }
+        });
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO make as not a match in database, get next potential match picture and info
+                potentialMatchPicture.setImageResource(R.drawable.housingicon);
+            }
+        });
     }
 
 }
