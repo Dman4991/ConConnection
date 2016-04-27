b0c917fec25e9350bafa553d7269e87c8027ea31
ianberger <spartan1179893@gmail.com>
2016-04-26 18:24:06 -0700

    Added take picture button to main activity for testing purposes.

--- app/src/main/java/com/example/daniel/conconnection/MainActivity.java
+++ app/src/main/java/com/example/daniel/conconnection/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.daniel.conconnection;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //TODO make as not a match in database, get next potential match picture and info
                 potentialMatchPicture.setImageResource(R.drawable.housingicon);
+            }
+        });
+
+
+        Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 11);
             }
         });
     }
