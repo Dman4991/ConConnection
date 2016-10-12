75672b25053aa34373bd95dd896619b5541c5bf5
Dman4991 <danielavetyan@gmail.com>
2016-10-11 20:44:13 -0700

    Added "Create New Account" xml and java files. Main Activity has remained the same, I tried something then reverted it back but it still considers it changed so I need to push it. Added a button to activity_login to create a new account. Edited a text field in fragment_account_management.xml and added a couple of string items in strings.xml.

--- app/src/main/java/com/example/daniel/conconnection/MainActivity.java
+++ app/src/main/java/com/example/daniel/conconnection/MainActivity.java
@@ -77,16 +77,6 @@ public class MainActivity extends AppCompatActivity
             });
         }
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -97,12 +87,12 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        //Display matching fragment by default
+        //Display MatchingFragment by default
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.content_frame, MatchingFragment.getInstance())
-                .commit();
-        getSupportActionBar().setTitle("Matches");
+                    .replace(R.id.content_frame, MatchingFragment.getInstance())
+                    .commit();
+            getSupportActionBar().setTitle("Matches");
     }
 
     @Override
